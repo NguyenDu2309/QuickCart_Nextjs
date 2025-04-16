32e1d662e2c5968c4b279428dae9cce04a42e948
Nguyen Du <nguyendu2004.anhuu@gmail.com>
2025-04-16 22:45:22 +0700

    json

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -11,8 +11,7 @@ const Footer = () => {
           <p className="mt-6 text-sm">
             Lorem Ipsum is simply dummy text of the printing and typesetting
             industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book.
+            ever since the 1500s
           </p>
         </div>
 
